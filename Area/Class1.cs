df99647bdf3b19187f78d1089c6010bbf87119b2
Олег Пересадин <operesadin66@outlook.com>
2023-04-23 18:50:21 +0300

    .

--- Area/Class1.cs
+++ Area/Class1.cs
@@ -2,38 +2,76 @@
 
 namespace Area
 {
- 
+    // Абстрактный класс Shape (фигура)
     public abstract class Shape
     {
+        // Абстрактный метод расчета площади фигуры
         public abstract double CalculateArea();
     }
+
+    // Класс Circle (круг)
     public class Circle : Shape
     {
+        // Радиус круга
         public double Radius { get; set; }
 
+        // Реализация метода расчета площади круга
         public override double CalculateArea()
         {
+            if (Radius < 0)
+            {
+                throw new ArgumentException("Радиус круга не может быть отрицательным.");
+            }
+
             return Math.PI * Radius * Radius;
         }
     }
+
+    // Класс Triangle (треугольник)
     public class Triangle : Shape
     {
+        // Длины сторон треугольника
         public double SideA { get; set; }
         public double SideB { get; set; }
         public double SideC { get; set; }
 
+        // Реализация метода расчета площади треугольника
         public override double CalculateArea()
         {
-            double s = (SideA + SideB + SideC) / 2;
-            if (SideA * SideA == SideB * SideB + SideC * SideC || SideB * SideB == SideA * SideA + SideC * SideC || SideC * SideC == SideA * SideA + SideB * SideB)
+            if (SideA < 0 || SideB < 0 || SideC < 0)
             {
-                Console.WriteLine("Треугольник прямоугольный.");
+                throw new ArgumentException("Длины сторон треугольника не могут быть отрицательными.");
             }
+
+            // Проверка валидности треугольника
+            if (!IsValid())
+            {
+                throw new ArgumentException("Треугольник с заданными сторонами не существует.");
+            }
+
+            // Вычисление полупериметра треугольника
+            double s = (SideA + SideB + SideC) / 2;
+
+            // Проверка прямоугольности треугольника
+            if (IsRightAngled())
+            {
+                Console.WriteLine("Треугольник является прямоугольным.");
+            }
+
+            // Вычисление площади треугольника по формуле Герона
             return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        // Проверка валидности треугольника
+        private bool IsValid()
+        {
+            return (SideA + SideB > SideC) && (SideA + SideC > SideB) && (SideB + SideC > SideA);
+        }
+
+        // Проверка прямоугольности треугольника
+        private bool IsRightAngled()
+        {
+            return SideA * SideA == SideB * SideB + SideC * SideC || SideB * SideB == SideA * SideA + SideC * SideC || SideC * SideC == SideA * SideA + SideB * SideB;
         }
     }
 }
-//Использовал для проверки работоспособности
-/*Shape shape = new Circle() { Radius = 5 }; // или new Triangle() { SideA = 3, SideB = 4, SideC = 5 };
-double area = shape.CalculateArea();
-Console.WriteLine(area);*/
